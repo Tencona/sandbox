5871a519baae2ca70f1bf0b7bdde297291835258
Conner Tennery <ctennery.dev@gmail.com>
2019-08-06 22:37:04 -0500

    Update README.md

--- src/views/StringSandbox.vue
+++ src/views/StringSandbox.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="stringSandbox">
-        <div class="title">Strings</div>
-        <div class="functions">
-            <SandboxFunction
-                class="sandboxFunction"
-                v-for="(func, i) in funcs"
-                :key="i"
-                :name="func.name"
-                :fields="func.fields"
-                :func="func.func"
-            />
-        </div>
-    </div>
+	<div class="stringSandbox">
+		<div class="title">Strings</div>
+		<div class="functions">
+			<SandboxFunction
+				class="sandboxFunction"
+				v-for="(func, i) in funcs"
+				:key="i"
+				:name="func.name"
+				:fields="func.fields"
+				:func="func.func"
+			/>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -19,22 +19,20 @@ import SandboxFunction from "../components/SandboxFunction.vue";
 let genString = require("../funcs/gen-string.js");
 
 export default {
-    name: "StringSandbox",
-    components: {
-        SandboxFunction
-    },
-    props: {},
-    data() {
-        return {};
-    },
-    computed: {
-        funcs() {
-            var s = "";
-            var aaa = "aaa";
-            return genString.funcs;
-        }
-    },
-    methods: {}
+	name: "StringSandbox",
+	components: {
+		SandboxFunction
+	},
+	props: {},
+	data() {
+		return {};
+	},
+	computed: {
+		funcs() {
+			return genString.funcs;
+		}
+	},
+	methods: {}
 };
 </script>
 
@@ -42,20 +40,20 @@ export default {
 .stringSandbox {
 }
 .title {
-    font-size: 48pt;
+	font-size: 48pt;
 }
 .functions {
-    display: flex;
-    justify-content: center;
-    margin-top: 18px;
-    overflow-y: auto;
-    scroll-behavior: smooth;
-    padding: 8px;
+	display: flex;
+	justify-content: center;
+	margin-top: 18px;
+	overflow-y: auto;
+	scroll-behavior: smooth;
+	padding: 8px;
 }
 .sandboxFunction {
-    transition: 0.2s;
+	transition: 0.2s;
 }
 .sandboxFunction:hover {
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+	box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
 }
 </style>
